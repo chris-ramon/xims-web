cd5644eb5c2180a911e21b39b551b0b66294276e
Christian Ramon <rchristian.ramon@gmail.com>
2014-04-07 17:41:32 -0500

    now sign in button shows loader

--- app/scripts/controllers/sessions.js
+++ app/scripts/controllers/sessions.js
@@ -4,13 +4,18 @@ angular.module('ximsApp')
   .controller('SignInCtrl', ['$scope', 'UserService', '$location',
     function($scope, UserService, $location) {
       $scope.userForm = {};
+      $scope.singingIn = false;
       $scope.signIn = function() {
+        $scope.singingIn = true;
         UserService.signIn($scope.userForm)
           .success(function() {
+            afterSignIn();
             $location.path('/');
           })
           .error(function() {
+            afterSignIn();
             $scope.flashMessage = 'Email or Password incorrect!.';
-        });
+          });
       }
+      function afterSignIn() { $scope.singingIn = false; }
   }]);

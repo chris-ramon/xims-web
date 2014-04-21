9e8435b1b8c6a86e902fd48bdeb5ccb132d03f1c
Christian Ramon <rchristian.ramon@gmail.com>
2014-04-21 13:18:41 -0500

    modularizing

--- app/scripts/lib/xims-Popover.js
+++ app/scripts/lib/xims-Popover.js
@@ -1,46 +1,52 @@
 'use strict';
 
-var ximsPopoverSelectController = function($scope, $element, PopoverService) {
-  var src = {};
-  $scope.PopoverService = PopoverService;
-  $scope.select = function() {
-    // we need to add new option
-    if($scope.PopoverService.optionSelected.id == 0) {
-      $scope.PopoverService.display = true;
-    } else {
-      // selected from the list
-      src[$scope.property] = $scope.PopoverService.optionSelected;
-      $scope.model = angular.extend($scope.model, src);
-    }
-  }
-};
-var ximsPopoverController = function($scope, $element, PopoverService) {
-  var src = {};
-  $scope.PopoverService = PopoverService;
-  $scope.$watch('PopoverService.display', displayPopover);
-  function displayPopover(newVal) {
-    if(newVal) { $($scope.selector).popover('show'); }
-  }
-  $scope.close = function(event, newOption) {
-    if(!newOption) { $scope.PopoverService.optionSelected = null; }
-    PopoverService.display = false;
-    src[$scope.property] = $scope.PopoverService.optionSelected;
-    $scope.model = angular.extend($scope.model, src);
-    $($scope.selector).popover('hide');
-    return false;
-  }
-};
-
 angular.module('ximsPopover', [])
+  .service('PopoverService', function() {
+    var self = this;
+    self.display = false;
+    self.optionSelected = {};
+  })
+  .controller('ximsPopoverSelectController', ['$scope', '$element', 'PopoverService',
+    function($scope, $element, PopoverService) {
+      var src = {};
+      $scope.PopoverService = PopoverService;
+      $scope.select = function() {
+      // we need to add new option
+      if($scope.PopoverService.optionSelected.id == 0) {
+        $scope.PopoverService.display = true;
+      } else {
+        // selected from the list
+        src[$scope.property] = $scope.PopoverService.optionSelected;
+        $scope.model = angular.extend($scope.model, src);
+      }
+    }
+  }])
+  .controller('ximsPopoverController', ['$scope', '$element', 'PopoverService',
+    function($scope, $element, PopoverService) {
+      var src = {};
+      $scope.PopoverService = PopoverService;
+      $scope.$watch('PopoverService.display', displayPopover);
+      function displayPopover(newVal) {
+        if(newVal) { $($scope.selector).popover('show'); }
+      }
+      $scope.close = function(event, newOption) {
+        if(!newOption) { $scope.PopoverService.optionSelected = null; }
+        PopoverService.display = false;
+        src[$scope.property] = $scope.PopoverService.optionSelected;
+        $scope.model = angular.extend($scope.model, src);
+        $($scope.selector).popover('hide');
+        return false;
+      }
+  }])
   .directive('ximsPopoverSelect', function() {
     return {
       restrict: 'E',
       scope: {items: '=', model: '=', property: '@'},
-      controller: ximsPopoverSelectController,
+      controller: 'ximsPopoverSelectController',
       template: '<select ng-model="PopoverService.optionSelected"\n        ng-options="item as item.name for item in items"\n        ng-change="select()" \n        class="form-control">\n</select>\n'
     }
   })
-  .directive('ximsPopover', function($templateCache, $compile) {
+  .directive('ximsPopover', ['$templateCache', '$compile', function($templateCache, $compile) {
     var getContent = function(scope, attrs) {
       var html = $templateCache.get(attrs.template);
       var template = angular.element(html);
@@ -56,18 +62,6 @@ angular.module('ximsPopover', [])
           placement: 'bottom', trigger: 'manual', html: true
         });
       },
-      controller: ximsPopoverController
+      controller: 'ximsPopoverController'
     }
-  })
-  .service('PopoverService', function() {
-    var self = this;
-    self.display = false;
-    self.optionSelected = {};
-  });
-
-
-
-
-
-
-
+  }]);

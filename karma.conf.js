5b606662f61727d8889b54fc4c81008a5c154e82
Christian Ramon <rchristian.ramon@gmail.com>
2014-04-22 17:06:00 -0500

    adding jquery and angular-ui

--- karma.conf.js
+++ karma.conf.js
@@ -11,12 +11,14 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'app/bower_components/jquery/dist/jquery.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/client/mock/**/*.js',

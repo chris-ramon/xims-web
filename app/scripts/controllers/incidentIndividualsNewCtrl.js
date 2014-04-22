e8b3ca420ed2fd5dc696d3fb77458d36abdfb3db
Christian Ramon <rchristian.ramon@gmail.com>
2014-04-22 17:03:35 -0500

    injecting IncidentStepsService

--- app/scripts/controllers/incidentIndividualsNewCtrl.js
+++ app/scripts/controllers/incidentIndividualsNewCtrl.js
@@ -1,19 +1,17 @@
 'use strict';
 
 angular.module('ximsApp')
-  .controller('IncidentIndividualsNewCtrl', ['$scope', 'ModuleService', function($scope, ModuleService) {
-    ModuleService.name = ModuleService.INCIDENT;
+  .controller('IncidentIndividualsNewCtrl', ['$scope', 'ModuleService', 'IncidentStepsService',
+    function($scope, ModuleService, IncidentStepsService) {
+      ModuleService.name = ModuleService.INCIDENT;
 
-    $scope.individuals = [
-      {id_number: 45996135, name: 'Chris Ramón'},
-      {id_number: 55667788, name: 'Luis Rodriguez'},
-      {id_number: 99900113, name: 'Carlos Dominguez'}
-    ];
+      $scope.individuals = [
+        {id_number: 45996135, name: 'Chris Ramón'},
+        {id_number: 55667788, name: 'Luis Rodriguez'},
+        {id_number: 99900113, name: 'Carlos Dominguez'}
+      ];
 
-//    $scope.setSelect = function($item, $model, $label) {
-//      $scope.selected = true;
-//    };
-
+      $scope.IncidentStepsService = IncidentStepsService;
   }])
   .controller('newEmployeeCtrl', ['$scope', function($scope) {
     $scope.save = function(event) {

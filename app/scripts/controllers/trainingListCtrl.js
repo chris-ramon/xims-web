809a7389495dec805b996fbf3f3610b2d01aed81
Christian Ramon <rchristian.ramon@gmail.com>
2014-04-28 16:56:26 -0500

    now training listing works

--- app/scripts/controllers/trainingListCtrl.js
+++ app/scripts/controllers/trainingListCtrl.js
@@ -1,6 +1,31 @@
 'use strict';
 
 angular.module('ximsApp')
-  .controller('TrainingListCtrl', ['$scope', 'ModuleService', function($scope, ModuleService) {
-    ModuleService.name = ModuleService.TRAINING;
+  .controller('TrainingListCtrl', ['$scope', '$filter', '$rootScope', 'ModuleService', 'TrainingService', 'UserService', 'AppSettings',
+    function($scope, $filter, $rootScope, ModuleService, TrainingService, UserService, AppSettings) {
+      ModuleService.name = ModuleService.TRAINING;
+      $scope.TrainingService = TrainingService;
+
+      $scope.maxSize = AppSettings.pagination.maxSize;
+      $scope.itemsPerPage = AppSettings.pagination.itemsPerPage;
+
+      $scope.searchText = "";
+
+      $scope.setTrainings = function(page) {
+        if($scope.searchText)
+          TrainingService.search($scope.searchText, page);
+        else
+          TrainingService.getAll(page);
+      };
+
+      $scope.$watch("searchText", filterEmployees);
+      function filterEmployees() {
+        var result = $filter('filter')($scope.TrainingService.trainingsCache, $scope.searchText);
+        if(result.length) { $scope.TrainingService.trainings = result; }
+      }
+
+      // when user refresh the page
+      $rootScope.$on('userLogged', function() { $scope.setTrainings(); });
+      // when user visits the module
+      if(UserService.currentUser) { $scope.setTrainings(); }
   }]);
--- app/scripts/services/training.js
+++ app/scripts/services/training.js
@@ -5,6 +5,47 @@ angular.module('ximsApp')
   .service('TrainingService', ['$http', 'UserService', 'ApiRoutes',
     function($http, UserService, ApiRoutes) {
       var self = this;
+      self.trainings = [];
+      self.trainingsCache = [];
+      self.totalItems = 0;
+      self.currentPage = 0;
+      self.loadingTrainings = false;
+
+      self.getAll = function(page) {
+        if(self.loadingTrainings) { return false; }
+        self.loadingTrainings = true;
+        return $http({
+          method: 'GET',
+          url: self.getAllUrl(),
+          params: {page: page}
+        }).success(self.trainingsSuccess);
+      };
+
+      self.getAllUrl = function() {
+        var url = ApiRoutes.getMainApiUrl() + '/:organization_id/trainings';
+        return url.replace(':organization_id',
+          UserService.currentUser.organization.id);
+      };
+
+      self.trainingsSuccess = function(response) {
+        self.trainings = response.data;
+        self.trainingsCache = response.data;
+        self.totalItems = response.meta.total_items;
+        self.currentPage = response.meta.current_page;
+        self.loadingTrainings = false;
+      };
+
+      self.search = function(term, page) {
+        if(self.loadingTrainings) { return false; }
+        self.loadingTrainings = true;
+        var _page = page || 1;
+        var url = ApiRoutes.getMainApiUrl() + '/search/trainings';
+        return $http({
+          url: url,
+          method: 'GET',
+          params: {term: term, page: _page}
+        }).success(self.trainingsSuccess);
+      };
 
       self.getTrainingsByEmployee = function(employeeId) {
         return $http({
